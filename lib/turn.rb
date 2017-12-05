bd2aa3795cdfbb5e53ad9f355a62b3aee59a7080
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-12-05 03:25:09 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move(board, index) == true
     move(board, index,)
+    display_board(board)
+    turn(board)
   else
     turn(board)
   end
